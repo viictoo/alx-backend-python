a7c91e11cbd5a2665d0abde8d5ec757f0aed4ad3
Victor Langat <provicml@gmail.com>
2024-01-11 12:51:44 +0300

    ✔added task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic annotations - add"""
+
+def add(a: float, b: float) -> float:
+    """type-annotated function add that takes 
+        a float a and a float bas arguments
+
+    Returns:
+        float: sum a b
+    """
+    return (a + b)

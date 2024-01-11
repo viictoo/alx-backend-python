c6b66de22cedb2ceb754d60b4cd17a4547253d5a
Victor Langat <provicml@gmail.com>
2024-01-11 12:57:35 +0300

    💨added task 2 floor

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a concatenated string from 2 str args
+
+    Args:
+        str1 (str): input string
+        str2 (str): input string
+
+    Returns:
+        str: concatenated string
+    """
+    return (str1 + str2)

eaae7793bb1d9a743627c7c3e595aaa5e7b6d97a
Victor Langat <provicml@gmail.com>
2024-01-11 12:54:01 +0300

    ✨pycodestyle fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Basic annotations - add"""
 
+
 def add(a: float, b: float) -> float:
-    """type-annotated function add that takes 
+    """type-annotated function add that takes
         a float a and a float bas arguments
 
     Returns:

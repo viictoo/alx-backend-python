c6f5183d500bb904a92b160c37f85813a78f9616
Victor Langat <provicml@gmail.com>
2024-01-11 12:56:23 +0300

    👉👈added task 1 concat

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float n
+
+    Args:
+        n (float): float
+
+    Returns:
+        int: floor(3.14) returns 3, which is a <class 'int'>
+    """
+    return (int(n))

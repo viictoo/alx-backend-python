34e9c7ef3835556969f43314326103bf9da6f269
Victor Langat <provicml@gmail.com>
2024-01-15 13:36:56 +0300

    🌠 task 0: fix docstring

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,11 +5,8 @@ import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> int:
+    """ waits for a random delay between 0 and max_delay
+    (included and float value)
+        seconds and eventually returns it
+    """
     return random.uniform(0, max_delay)
-
-
-if __name__ == '__main__':
-    import asyncio
-    print(asyncio.run(wait_random()))
-    print(asyncio.run(wait_random(5)))
-    print(asyncio.run(wait_random(15)))

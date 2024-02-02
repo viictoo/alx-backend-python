9efa61367bf5a96e897aee705dec35918555df11
Victor Langat <provicml@gmail.com>
2024-02-02 18:21:33 +0300

    test requirements v11

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,60 +31,56 @@ class TestAccessNestedMap(TestCase):
     ])
     def test_access_nested_map_exception(
         self, name: str, nested_map: Mapping, path: Sequence
-    ) -> None:
+    ):
         """tests for access_nested_map function method"""
         with (self.assertRaises(KeyError)):
             access_nested_map(nested_map, path)
 
 
-# class TestGetJson(TestCase):
-#     """unittests for the access nested map function
+class TestGetJson(TestCase):
+    """unittests for the access nested map function
 
-#     Args:
-#         TestCase (class): A class whose instances are single test cases.
-#     """
-#     @parameterized.expand([
-#         ("test_example", "http://example.com", {"payload": True}),
-#         ("test_holberton", "http://holberton.io", {"payload": False}),
-#     ])
-#     @mock.patch("utils.requests.get")
-#     def test_get_json(self,
-#                       name: str,
-#                       url: str,
-#                       test_response: dict,
-#                       mock_requests: Any
-#                       ) -> None:
-#         """assert that the get json method calls the url presented"""
-#         mock_requests.return_value.json.return_value = test_response
-#         self.assertEqual(get_json(url), test_response)
-#         mock_requests.assert_called_once_with(url)
+    Args:
+        TestCase (class): A class whose instances are single test cases.
+    """
+    @parameterized.expand([
+        ("test_example", "http://example.com", {"payload": True}),
+        ("test_holberton", "http://holberton.io", {"payload": False}),
+    ])
+    @mock.patch("utils.requests.get")
+    def test_get_json(
+        self, name: str, url: str, test_response: dict, mock_requests: Any) -> None:
+        """assert that the get json method calls the url presented"""
+        mock_requests.return_value.json.return_value = test_response
+        self.assertEqual(get_json(url), test_response)
+        mock_requests.assert_called_once_with(url)
 
 
-# class TestMemoize(unittest.TestCase):
-#     """unittests for the memoize function
+class TestMemoize(unittest.TestCase):
+    """unittests for the memoize function
 
-#     Args:
-#         TestCase (class): A class whose functions are single test cases
-#     """
+    Args:
+        TestCase (class): A class whose functions are single test cases
+    """
 
-#     def test_memoize(self):
-#         """test method for  utils.memoize method
-#         """
-#         class TestClass:
-#             """ test class with 2 methods"""
+    def test_memoize(self):
+        """test method for  utils.memoize method
+        """
+        class TestClass:
+            """ test class with 2 methods"""
 
-#             def a_method(self) -> int:
-#                 """this method cachreturns 42"""
-#                 return 42
+            def a_method(self) -> int:
+                """this method cachreturns 42"""
+                return 42
 
-#             @memoize
-#             def a_property(self) -> Any:
-#                 """method with a cached result"""
-#                 return self.a_method()
+            @memoize
+            def a_property(self) -> Any:
+                """method with a cached result"""
+                return self.a_method()
 
-#         with mock.patch.object(TestClass, "a_method") as mock_method:
-#             test_class = TestClass()
-#             for _ in range(7):
-#                 test_class.a_property
-#             # self.assertEqual(test_class.a_property(), 42)|returns mock id
-#             mock_method.assert_called_once()
+        with mock.patch.object(TestClass, "a_method") as mock_method:
+            test_class = TestClass()
+            for _ in range(7):
+                test_class.a_property
+            # self.assertEqual(test_class.a_property(), 42)|returns mock id
+            mock_method.assert_called_once()

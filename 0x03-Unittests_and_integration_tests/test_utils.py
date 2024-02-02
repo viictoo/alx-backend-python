fda941ba4fc8764f91fdfba16a985caddf8681b6
Victor Langat <provicml@gmail.com>
2024-02-02 18:07:34 +0300

    test imports v8

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
+"""UNIT & INTEGRATION TESTS MODULE FOR UTILS
 """
-Test suite for utils.py
-"""
-
 import unittest
-from unittest.mock import patch
-from typing import Mapping, Sequence, Any
-
+from unittest import TestCase, mock
 from parameterized import parameterized
-
-from utils import access_nested_map, get_json, memoize
+from typing import Mapping, Sequence, Any
+from utils import access_nested_map as accessMap, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
